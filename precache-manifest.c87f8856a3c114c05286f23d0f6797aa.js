a0ed49ed4933647cc90f876e2d90173037a41950
Sergey Podlozny <spodlozny@qulix.com>
2019-11-21 15:19:36 +0300

    Updates

---
+++ precache-manifest.c87f8856a3c114c05286f23d0f6797aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e777b780044e1ad0b3b970d39fc842c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9fe07b6c4b0f8222fa29",
+    "url": "./static/css/main.aa8fe618.chunk.css"
+  },
+  {
+    "revision": "f5ef16194de6b12f441f",
+    "url": "./static/js/2.9f098cc4.chunk.js"
+  },
+  {
+    "revision": "9fe07b6c4b0f8222fa29",
+    "url": "./static/js/main.42e42f2e.chunk.js"
+  },
+  {
+    "revision": "4a840a3c7a7ec9b749b2",
+    "url": "./static/js/runtime-main.dd7ee2fd.js"
+  }
+]);
